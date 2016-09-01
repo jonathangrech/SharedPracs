c76804fc23033ef0db540e68f13927051c067f48
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-01 19:18:03 +1000

    print all colours and codes iteratively with string formatting

--- Prac05/colourNames.py
+++ Prac05/colourNames.py
@@ -1,8 +1,11 @@
 """
 CP1404/CP5632 Practical
-State colours in a dictionary
+Colour codes in a dictionary
 """
 
 COLOUR_CODES = {"AliceBlue": "#f0f8ff", "DarkGreen": "#006400", "GreenYellow": "#adff2f", "HotPink": "#ff69b4",
                 "IndianRed": "#cd5c5c", "MediumSpringGreen": "#00fa9a", "MintCream": "#f5fffa",
                 "PaleTurquoise": "#afeeee", "SaddleBrown": "#8b4513", "SteelBlue": "#4682b4"}
+
+for colour in COLOUR_CODES:
+    print("{:<20} is {}".format(colour, COLOUR_CODES[colour]))
