4586dd99ac2d3e81d7cd2980f70c0968079b46e9
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-01 19:14:50 +1000

    choose colours and add to dictionary

---
+++ Prac05/colourNames.py
@@ -0,0 +1,3 @@
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "DarkGreen": "#006400", "GreenYellow": "#adff2f", "HotPink": "#ff69b4",
+                "IndianRed": "#cd5c5c", "MediumSpringGreen": "#00fa9a", "MintCream": "#f5fffa",
+                "PaleTurquoise": "#afeeee", "SaddleBrown": "#8b4513", "SteelBlue": "#4682b4"}
