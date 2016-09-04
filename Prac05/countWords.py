ff1f9d5542d9eb071764a29a1a05008f140b5c56
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-04 23:49:49 +1000

    extract dictionary entries into list, print pairs formatted based on width of largest key

--- Prac05/countWords.py
+++ Prac05/countWords.py
@@ -9,6 +9,16 @@ for word in list_of_words:
     else:
         word_dict[word] = 1
 
+word_lengths = []
+words = sorted(list(word_dict.keys()))
+for word in words:
+    word_lengths.append(len(word))
 
-for word in word_dict:
-    print("{} : {}".format(word, word_dict[word]))
+width = max(word_lengths)
+
+word_counts = list(word_dict.items())
+word_counts = sorted(word_counts, key=lambda x: x[0])
+
+for word in word_counts:
+    print("{:<{width}} : {}".format(word[0], word[1], width=width))
+
