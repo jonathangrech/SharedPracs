7ae5fde21ad30b1534bd0876503a6a0e1d360ef7
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-14 10:27:54 +1000

    simplify demo file

---
+++ Prac06/loop_list.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+
+class loopListApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        # Construct main app
+        super().__init__(**kwargs)
+        # basic data example - dictionary of names: phone numbers
+        self.names= ["Jack", "Jenny", "Peter", "Pauline", "Chris", "Ryan"]
+
+    def build(self):
+        # Build Kivy GUI
+        self.title = "Loop Through Names"
+        self.root = Builder.load_file('loop_list.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            self.root.ids.entriesBox.add_widget(Label(text='[color=ff69b4]{}'.format(name), markup=True))
+
+
+loopListApp().run()
