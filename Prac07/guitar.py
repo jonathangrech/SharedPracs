e61a9548859a8eb8b831789674ef82bccb6c09d5
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-20 00:25:16 +1000

    define init and str methods

---
+++ Prac07/guitar.py
@@ -0,0 +1,8 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:.2f}"
