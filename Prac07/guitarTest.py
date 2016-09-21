a8f32012405b6bf0a21454dc44078979ac1212bd
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-21 19:57:07 +1000

    print stored objects in given format and based on vintage status

--- Prac07/guitarTest.py
+++ Prac07/guitarTest.py
@@ -10,10 +10,25 @@ def main():
             break
         year = int(input("Year: "))
         cost = float(input("Cost: $"))
-
-        guitars.append(Guitar(name, year, cost))
-        print(guitars[i])
+        guitar_object = Guitar(name, year, cost)
+        if guitar_object.is_vintage():
+            guitars.append([Guitar(name, year, cost), "Y"])
+        else:
+            guitars.append([Guitar(name, year, cost), "N"])
+        print(guitar_object)
         i += 1
+
+    print("These are my guitars:")
+    for guitar_number in range(0,len(guitars)):
+        if guitars[guitar_number][1] == "Y":
+            print("Guitar {}: {:>25} ({}), worth ${:>12,.2f} (vintage)".format(guitar_number + 1,
+                                                                              guitars[guitar_number][0].name,
+                                                                              guitars[guitar_number][0].year,
+                                                                              guitars[guitar_number][0].cost))
+        else:
+            print("Guitar {}: {:>25} ({}), worth ${:>12,.2f}".format(guitar_number + 1, guitars[guitar_number][0].name,
+                                                                    guitars[guitar_number][0].year,
+                                                                    guitars[guitar_number][0].cost))
 
 
     # gibson = Guitar("Gibson", 2011, 16035.4)
