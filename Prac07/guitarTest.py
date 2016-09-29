4f6f96fc985f064c9aeed8fd80815fd84dc1594d
jonathangrech <jonathan.grech@my.jcu.edu.au>
2016-09-29 13:36:36 +1000

    fix formatting as per feedback

--- Prac07/guitarTest.py
+++ Prac07/guitarTest.py
@@ -2,8 +2,6 @@ from Prac07.guitar import Guitar
 
 
 def main():
-
-    i = 0
     guitars = []
     while True:
         name = input("Name: ")
@@ -14,18 +12,17 @@ def main():
         guitar_object = Guitar(name, year, cost)
         guitars.append(guitar_object)
         print(guitar_object)
-        i += 1
 
     print("These are my guitars:")
-    for guitar_number in range(0,len(guitars)):
+    for guitar_number in range(0, len(guitars)):
         if guitars[guitar_number].is_vintage():
             vintage_string = "(vintage)"
         else:
             vintage_string = ""
 
-        print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format(guitar_number + 1,
-                                                                           guitars[guitar_number].name,
-                                                                           guitars[guitar_number].year,
-                                                                           guitars[guitar_number].cost, vintage_string))
+        print("Guitar {}: {:20} ({}), worth ${:10,.2f} {}".format(guitar_number + 1, guitars[guitar_number].name,
+                                                                  guitars[guitar_number].year,
+                                                                  guitars[guitar_number].cost, vintage_string))
+
 
 main()
